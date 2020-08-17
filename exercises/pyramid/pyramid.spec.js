aaa73053f1c3fc14966810d59d3e4a70cde8af7e
Keionne Derousselle <6044786+KeionneDerousselle@users.noreply.github.com>
2020-08-17 09:06:04 -0600

    starting pyramid problem

---
+++ exercises/pyramid/pyramid.spec.js
@@ -0,0 +1,39 @@
+const pyramid = require('./index')
+
+describe('Pyramid', () => {
+  beforeEach(() => {
+    console.log = jest.fn().mockImplementation(() => {})
+  })
+
+  afterEach(() => {
+    console.log.mockRestore()
+  })
+
+  it('should define pyramid as a function', () => {
+    expect(typeof pyramid).toEqual('function')
+  })
+
+  it('should print a pyramid for n = 2', () => {
+    pyramid(2)
+    expect(console.log.mock.calls[0][0]).toEqual(' # ')
+    expect(console.log.mock.calls[1][0]).toEqual('###')
+    expect(console.log.mock.calls.length).toEqual(2)
+  })
+
+  it('should print a pyramid for n = 3', () => {
+    pyramid(3)
+    expect(console.log.mock.calls[0][0]).toEqual('  #  ')
+    expect(console.log.mock.calls[1][0]).toEqual(' ### ')
+    expect(console.log.mock.calls[2][0]).toEqual('#####')
+    expect(console.log.mock.calls.length).toEqual(3)
+  })
+
+  it('should print a pyramid for n = 4', () => {
+    pyramid(4)
+    expect(console.log.mock.calls[0][0]).toEqual('   #   ')
+    expect(console.log.mock.calls[1][0]).toEqual('  ###  ')
+    expect(console.log.mock.calls[2][0]).toEqual(' ##### ')
+    expect(console.log.mock.calls[3][0]).toEqual('#######')
+    expect(console.log.mock.calls.length).toEqual(4)
+  })
+})
