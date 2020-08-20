af10c58f36fdfdd4c2a4967d94211fcb1ae61758
Keionne Derousselle <6044786+KeionneDerousselle@users.noreply.github.com>
2020-08-19 22:14:58 -0600

    fibonnaci problem

---
+++ solutions/fib/fib.spec.js
@@ -0,0 +1,28 @@
+const { fib1, fib2 } = require('./index')
+const fibFns = [ fib1, fib2 ]
+
+describe.each(fibFns)('Fib - %p', fib => {
+  it('should define Fib as a function', () => {
+    expect(typeof fib).toEqual('function')
+  })
+
+  it('should return 1 when calculating the fib value for 1', () => {
+    expect(fib(1)).toEqual(1)
+  })
+
+  it('should return 1 when calculating the fib value for 2', () => {
+    expect(fib(2)).toEqual(1)
+  })
+
+  it('should return 2 when calculating the fib value of 3', () => {
+    expect(fib(3)).toEqual(2)
+  })
+
+  it('should return 3 when calculating the fib value of 4', () => {
+    expect(fib(4)).toEqual(3)
+  })
+
+  it('should return 63245986 when calculating the fib value for 39', () => {
+    expect(fib(39)).toEqual(63245986)
+  })
+})
---
+++ solutions/fib/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceding two.
+
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+
+// Example:
+//   fib(4) === 3
+
+// ----- MY SOLUTION ----- O(n)
+const fib1 = n => {
+  if (n === 1) return 1
+
+  const seq = [ 0, 1 ]
+
+  for (let i = 2; i <= n; i++) {
+    seq.push(seq[i - 2] + seq[i - 1])
+  }
+
+  return seq[n]
+}
+
+// ----- SOLUTION 2 ------
+const fib2 = n => {
+  if (n === 1) return 1
+  if (n <= 0) return 0
+
+  return fib2(n - 2) + fib2(n - 1)
+}
+
+module.exports = {
+  fib1,
+  fib2
+}
